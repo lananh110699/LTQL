99df28cfa0190b4eeebeff8043f766fe40b7c111
NGUYEN THI LAN ANH <1721050613@student.humg.edu.vn>
2021-09-25 15:49:08 +0700

    uppp

--- Controllers/StdNewController.cs
+++ Controllers/StdNewController.cs
@@ -8,7 +8,7 @@ using LTQL.Models;
 
 namespace LTQL.Controllers
 {
-    
+
     public class StdNewController : Controller
     {
         LTQLDBContext db = new LTQLDBContext();
@@ -22,16 +22,36 @@ namespace LTQL.Controllers
 
         public ActionResult Create()
         {
+            var stdID = "";
+            var countStudent = db.Students.Count();
+            if (countStudent == 0)
+            {
+                stdID = "STD001";
+            }
+            else
+            {
 
+                var studentID = db.Students.ToList().OrderByDescending(m => m.StudentID).FirstOrDefault().StudentID;
+
+                stdID = genKey.GenerateKey(studentID);
+            }
+            ViewBag.StudentID = stdID;
             return View();
         }
         [HttpPost]
-        public ActionResult Create (Student std)
+        public ActionResult Create(Student std)
         {
-            var countStudent = db.Students.Count();
-            if (countStudent==0)
 
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+
+                db.Students.Add(std);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(std);
         }
     }
 }
+    
--- Models/StringProcess.cs
+++ Models/StringProcess.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace LTQL.Models
 {
     public class StringProcess
     {
-
+        public string GenerateKey(string id)
+        {
+            string strkey = "";
+            string numPart = "", strPart = "", strPhanSo = "";
+            numPart = Regex.Match(id, @"\d+").Value;
+            
+            //them ca so o de kich thuoc = phan so => 1+1 = 2
+            int phanso = (Convert.ToInt32(numPart) + 1);
+            for (int i = 0; i < numPart.Length - phanso.ToString().Length; i++)
+            {
+                strPhanSo += "0";
+            }
+            strPhanSo += phanso;
+            strkey = strPart + strPhanSo;
+            return strkey;
+        }
     }
 }
