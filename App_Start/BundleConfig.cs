c101646548fada2b0689f8367d4cb30e44b2b94e
NGUYEN THI LAN ANH <1721050613@student.humg.edu.vn>
2021-09-28 14:19:04 +0700

    up

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -10,6 +10,8 @@ namespace LTQL
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/ckEditor").Include(
+                        "~/Scripts/ckeditor/ckeditor.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,7 +26,8 @@ namespace LTQL
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                        "~/Content/W3.css"));
         }
     }
 }
